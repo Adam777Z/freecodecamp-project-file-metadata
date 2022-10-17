99ad61198f010bee9391cd732da4083124ea285d
Adam777 <Adam777-5867@outlook.com>
2022-10-17 21:52:15 +0000

    Update

--- server.js
+++ server.js
@@ -12,25 +12,25 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  return res.json({greetings: "Hello, API"});
+	res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
-  if (req.file !== undefined) {
-    return res.json({
-      name: req.file.originalname,
-      type: req.file.mimetype,
-      size: req.file.size
-    });
-  } else {
-    return res.json({ error: 'file is required' });
-  }
+app.get('/hello', function (req, res) {
+	return res.json({ greetings: 'Hello, API' });
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+	if (req.file !== undefined) {
+		return res.json({
+			name: req.file.originalname,
+			type: req.file.mimetype,
+			size: req.file.size
+		});
+	} else {
+		return res.json({ error: 'file is required' });
+	}
 });
 
 const listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening on port ' + listener.address().port);
+	console.log('Node.js listening on port ' + listener.address().port);
 });
